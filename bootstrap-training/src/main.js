f203fbf131b9bece5f3dd4891cbe8ffef908ed4b
SanskritiAcharya <sanskriti.acharyaaa27@gmail.com>
2026-06-03 19:25:57 +0545

    Fix: css was not loading

--- bootstrap-training/src/main.js
+++ bootstrap-training/src/main.js
@@ -1,14 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'swiper/css';
-import './styles.css';
-
-import Swiper from 'swiper';
-import { Autoplay } from 'swiper/modules';
-
 // Swiper
 const swiper = new Swiper('.overview-swiper', {
-  modules: [Autoplay],
   loop: true,
   autoplay: { delay: 3000, disableOnInteraction: false },
   on: {
